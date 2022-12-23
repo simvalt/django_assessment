297ded4684666b75776bb8e00a696f14429f86b9
Arturo Islas <arthur.islas2015@gmail.com>
2022-12-22 23:28:15 -0600

    Refs #2 Added models to admin site

--- django_test/webApp/admin.py
+++ django_test/webApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Customers, Payments_customers, User
 
 # Register your models here.
+admin.site.register(Customers)
+admin.site.register(Payments_customers)
+admin.site.register(User)
