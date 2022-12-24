f0dece63c55193c14a7df149bb049f4aa343490d
Arturo Islas <arthur.islas2015@gmail.com>
2022-12-24 00:00:02 -0600

    Refs #3 Created view for customers

--- django_test/webApp/views.py
+++ django_test/webApp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Customers
 
 # Create your views here.
+def home(request):
+    customers = Customers.objects.all()
+    return render(request, "customers.html", {"customers": customers})
+
+def registrarCustomers(request):
+    id = request.POST['txtid']
+    name = request.POST['txtNombre']
+    paternal_surname = request.POST['txtApellido']
+    email = request.POST['txtEmail']
+
+    customer=Customers.objects.create(id=id, name=name, paternal_surname=paternal_surname, email=email)
+
+    return redirect('/')
