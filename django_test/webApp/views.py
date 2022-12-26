dcebe391f2c275f7c1432eee1aed4b3d4f0ddf69
Arturo Islas <arthur.islas2015@gmail.com>
2022-12-26 15:40:40 -0600

    Refs #3 Added functions for update and delete in customers view

--- django_test/webApp/views.py
+++ django_test/webApp/views.py
@@ -15,3 +15,27 @@ def registrarCustomers(request):
     customer=Customers.objects.create(id=id, name=name, paternal_surname=paternal_surname, email=email)
 
     return redirect('/')
+
+def editarCustomer(request, id):
+    customer = Customers.objects.get(id=id)
+    return render(request, "editarCustomer.html", {"customer": customer})
+
+def editarCustomers(request):
+    id = request.POST['txtid']
+    name = request.POST['txtNombre']
+    paternal_surname = request.POST['txtApellido']
+    email = request.POST['txtEmail']
+
+    customer = Customers.objects.get(id=id)
+    customer.name = name
+    customer.paternal_surname = paternal_surname
+    customer.email = email
+    customer.save()
+
+    return redirect('/')
+
+def eliminarCustomer(request, id):
+    Customer = Customers.objects.get(id=id)
+    Customer.delete()
+
+    return redirect('/')
