e46aca5f87217fc2d4f33b258be7ae64c9e92496
Arturo Islas <arthur.islas2015@gmail.com>
2022-12-26 15:39:38 -0600

    Refs #3 Added urls for edit, update and delete for customers

--- django_test/webApp/urls.py
+++ django_test/webApp/urls.py
@@ -5,4 +5,7 @@ from webApp.views import home
 urlpatterns = [
     path('', views.home),
     path('registrarCustomers/', views.registrarCustomers),
+    path('editarCustomer/<id>', views.editarCustomer),
+    path('editarCustomers/', views.editarCustomers),
+    path('eliminarCustomer/<id>', views.eliminarCustomer),
 ]
